249c62d261cf558b52cfccf74d17c89b136937b9
LukaszKruszynski <lukaszkruszynski96@gmail.com>
2021-01-15 15:22:07 +0100

    clean code refactor

--- src/main/java/pl/kruszynski/currencycalculator/service/TableCurrencyService.java
+++ src/main/java/pl/kruszynski/currencycalculator/service/TableCurrencyService.java
@@ -1,9 +1,6 @@
 package pl.kruszynski.currencycalculator.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import pl.kruszynski.currencycalculator.client.TableCurrencyClient;
 import pl.kruszynski.currencycalculator.domain.Currency;
@@ -47,14 +44,11 @@ public class TableCurrencyService {
                 .collect(Collectors.toMap(e -> e, Currency::getMid));
     }
 
-
     public BigDecimal calculateRates(DataCalculateRate dataCalculateRate) {
         BigDecimal currencyValue = this.findCurrencyValue(dataCalculateRate.getCurrencyAmountCode());
         BigDecimal currencyValueToCalculate = this.findCurrencyValue(dataCalculateRate.getCurrencyAmountCodeToCalculate());
         BigDecimal toPLN = calculateToPLN(dataCalculateRate.getAmount(), currencyValue);
-        BigDecimal calculateResult = toPLN.divide(currencyValueToCalculate, RoundingMode.HALF_UP);
-        return calculateResult;
-
+        return toPLN.divide(currencyValueToCalculate, RoundingMode.HALF_UP);
     }
 
     public BigDecimal calculateToPLN(BigDecimal amount, BigDecimal currencyValue) {
@@ -66,8 +60,7 @@ public class TableCurrencyService {
                 .filter(e -> e.getKey().getCode().equals(currencyAmountCode.toUpperCase()))
                 .map(e -> e.getKey().getMid())
                 .collect(Collectors.toList());
-        BigDecimal currencyValue = currencyAmountValueList.stream().findFirst().orElse((BigDecimal.ZERO));
-        return currencyValue;
+        return currencyAmountValueList.stream().findFirst().orElse((BigDecimal.ZERO));
     }
 
     public Map<Currency, BigDecimal> getCurrencies() {
